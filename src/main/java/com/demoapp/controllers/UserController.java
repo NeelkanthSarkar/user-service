f2306c5052de47611c46c8328e1c2c19f71f9b6e
Neelkanth Sarkar <neelkanthsarkar6@gmail.com>
2023-10-28 10:27:55 +0530

    removed spring security dependency from pom.xml

--- src/main/java/com/demoapp/controllers/UserController.java
+++ src/main/java/com/demoapp/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.demoapp.controllers;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +23,8 @@ import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
+	
+	
 	
 	@Autowired
 	private UserServiceImpl userServiceImpl;
